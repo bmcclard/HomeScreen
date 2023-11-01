2d3cd27c788e71dd5b381d3dfb2ed84aad797c47
bmcclard <145383099+bmcclard@users.noreply.github.com>
2023-11-01 10:29:54 -0600

    1 Nov

---
+++ HomeScreen/HomeScreen/Controller/InfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InfoViewController.swift
+//  HomeScreen
+//
+//  Created by Bradley McClard on 10/31/23.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+
+    }
+    
+}
